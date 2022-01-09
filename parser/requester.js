b87416e0cea330a947873b9669e4731ca79b1773
dodgervl <vpslayer30@gmail.com>
2022-01-09 15:45:02 +0300

    Added logic to extract meta information for uri link for NFTs

--- parser/requester.js
+++ parser/requester.js
@@ -11,25 +11,22 @@ async function getRequest(url) {
   }
 }
 
-// Return meta information for given tokenAddress (symbol,name, website...) from solscan.io
+// Returns meta information for a given tokenAddress (symbol,name, website...) from solscan.io
 async function solscanGetTokenMetaInfo(tokenAddress) {
-  console.log(tokenAddress)
   let url = `https://public-api.solscan.io/token/meta?tokenAddress=${tokenAddress}`
   const response = await getRequest(url)
   return response
 }
 
-// Return price and volume for given tokenAddress from solscan.io (which get it from coingecko.com)
+// Returns price and volume for a given tokenAddress from solscan.io (which in turn gets it from coingecko.com)
 async function solscanGetTokenMarketInfo(tokenAddress) {
-  console.log(tokenAddress)
   let url = `https://public-api.solscan.io/market/token/${tokenAddress}`
   const response = await getRequest(url)
   return response
 }
 
-// Return NFT meta and price information for given walletAddress from solanart.io (ONLY if NFT is listed on solanart.io)
+// Returns NFT meta and price information for a given walletAddress from solanart.io (ONLY if NFT is listed on solanart.io)
 async function solanartGetWalletNFT(walletAddress) {
-  console.log(walletAddress)
   let url = `https://qzlsklfacc.medianetwork.cloud/infos_wallet_for_sale?address=${walletAddress}`
   const response = await getRequest(url)
   return response
@@ -38,9 +35,13 @@ async function solanartGetWalletNFT(walletAddress) {
 // To test on "devnet" need to change "mainnet-beta" in node_modules/@nfteyez/sol-rayz/dist/utils.js on line:
 // if (clusterApi === void 0) { clusterApi = (0, web3_js_1.clusterApiUrl)("mainnet-beta"); }
 // to "devnet"
-// Return NFT information for given walletAddress
+// Returns information about solana NFTS in a given walletAddress.
 async function getWalletNFT(walletAddress) {
   const tokenList = await moduleSolRayz.getParsedNftAccountsByOwner({'publicAddress':walletAddress});
+  for (var i = 0; i < tokenList.length; i++) {
+    let url =  tokenList[i].data.uri
+    tokenList[i]['metaData'] = await getRequest(url)
+   }
   return tokenList
 }
 
